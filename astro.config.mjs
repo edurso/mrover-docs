89db618a6b988dd93fbfaa040114c94e76bef1f8
Eric D'Urso <edurso@edurso.com>
2026-09-03 19:26:21 -0400

    fix path issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,7 @@ import rehypeKatex from 'rehype-katex';
 import { unified } from '@astrojs/markdown-remark';
 
 export default defineConfig({
-  // site: 'https://docs.mrover.org',
-  site: 'https://edurso.github.com/mrover-docs/',
-  base: '/mrover-docs',
+  site: 'https://docs.mrover.org',
   markdown: {
     processor: unified({ remarkPlugins: [remarkMath], rehypePlugins: [rehypeKatex] }),
   },
